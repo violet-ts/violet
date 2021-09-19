c58baec0a1042ba7c3966938f96d801cdfe84da5
mai_hirose <wind.close.cloud.girl@gmail.com>
2021-09-19 14:19:50 +0900

    feat: Fix Icon after review

--- pages/browser/[...pathes]/components/StreamBar/index.tsx
+++ pages/browser/[...pathes]/components/StreamBar/index.tsx
@@ -9,14 +9,13 @@ const Container = styled.div`
 `
 const StreamBox = styled.div`
   flex: 1;
-  height: 100%;
 `
 const MessageBox = styled.div`
   padding: 8px;
 `
 const InputForm = styled.input`
   width: 100%;
-  height: 80px;
+  min-height: 80px;
   border: 1px solid ${colors.violet}${alphaLevel[2]};
   ::placeholder {
     color: ${colors.violet}${alphaLevel[2]};
@@ -28,12 +27,11 @@ const SubmitIcon = styled.button`
   bottom: 24px;
   width: 16px;
   height: 4px;
-  margin-right: -2px;
-  border-right: 2px solid transparent;
+  border-right: solid transparent;
   border-top-right-radius: 1px;
   border-bottom-right-radius: 1px;
   box-shadow: 0 0 0 2px, inset -2px 0 0;
-  transform: rotate(-45deg) scale(var(--ggs, 1));
+  transform: rotate(-45deg);
   ::before {
     position: absolute;
     top: -4px;
