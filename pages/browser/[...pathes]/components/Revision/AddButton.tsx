614576b4cea3f0a6c99324ad2c78bbdf8c9e890a
mai_hirose <wind.close.cloud.girl@gmail.com>
2021-10-23 23:39:56 +0900

    feat: Create AddButton

---
+++ pages/browser/[...pathes]/components/Revision/AddButton.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+import { alphaLevel, colors } from '~/utils/constants'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
+const Adjustments = styled.div`
+  height: 95%;
+`
+const StyledAddButton = styled.button`
+  & {
+    position: relative;
+    box-sizing: border-box;
+    display: block;
+    width: 64px;
+    height: 64px;
+    color: ${colors.violet};
+    background-color: ${colors.violet}${alphaLevel[2]};
+    border: 2px solid;
+    border-radius: 4px;
+    transform: scale(var(--ggs, 1));
+  }
+  &::after,
+  &::before {
+    position: absolute;
+    top: 30px;
+    left: 18px;
+    box-sizing: border-box;
+    display: block;
+    width: 24px;
+    height: 4px;
+    color: ${colors.violet};
+    content: '';
+    background: currentColor;
+    border-radius: 5px;
+  }
+  &::after {
+    top: 20px;
+    left: 28px;
+    width: 4px;
+    height: 24px;
+    color: ${colors.violet};
+  }
+`
+export const AddButton = () => {
+  return (
+    <Container>
+      <Adjustments />
+      <StyledAddButton />
+    </Container>
+  )
+}
