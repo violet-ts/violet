f5f936500b5949f72ee4bf75140bdcda970ed481
Luma <world@luma.email>
2021-10-27 01:17:53 +0900

    fix: about credentials

--- server/service/aws-credential.ts
+++ server/service/aws-credential.ts
@@ -1,9 +1,13 @@
-import { fromContainerMetadata, fromEnv } from '@aws-sdk/credential-providers'
-import { AWS_ACCESS_KEY_ID } from '../utils/envValues'
+import { fromContainerMetadata } from '@aws-sdk/credential-providers'
+import type { Credentials, Provider } from "@aws-sdk/types";
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from '../utils/envValues'
 
-export const getCredentials = () => {
+export const getCredentials = (): Credentials | Provider<Credentials> => {
   if (AWS_ACCESS_KEY_ID) {
-    return fromEnv()
+    return {
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    }
   }
   return fromContainerMetadata()
 }
