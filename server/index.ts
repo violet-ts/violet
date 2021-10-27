699e1da80a82815036feeafdc5f769706e50e7b1
Luma <world@luma.email>
2021-10-27 16:58:59 +0900

    chore: typecheck, remove fastify-jwt and fastify-static

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,9 @@
 import Fastify from 'fastify'
 import cors from 'fastify-cors'
 import helmet from 'fastify-helmet'
-import fastifyJwt from 'fastify-jwt'
-import fastifyStatic from 'fastify-static'
-import path from 'path'
 import server from './$server'
 import { createBucketIfNotExists } from './service/s3'
-import { BASE_PATH, JWT_SECRET, SERVER_PORT } from './utils/envValues'
+import { BASE_PATH, SERVER_PORT } from './utils/envValues'
 
 const fastify = Fastify()
 
@@ -18,10 +15,5 @@ const startServer = async () => {
 
 fastify.register(helmet)
 fastify.register(cors)
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
-  prefix: BASE_PATH,
-})
-fastify.register(fastifyJwt, { secret: JWT_SECRET })
 
 startServer()
--- server/utils/envValues.ts
+++ server/utils/envValues.ts
@@ -2,9 +2,6 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const JWT_SECRET = process.env.JWT_SECRET ?? ''
-const USER_ID = process.env.USER_ID ?? ''
-const USER_PASS = process.env.USER_PASS ?? ''
 const SERVER_PORT = +(process.env.SERVER_PORT ?? '8080')
 const BASE_PATH = process.env.BASE_PATH ?? ''
 const API_ORIGIN = process.env.API_ORIGIN ?? ''
@@ -16,9 +13,6 @@ const S3_BUCKET = process.env.S3_BUCKET ?? 'violet-app'
 const NODE_ENV = process.env.NODE_ENV ?? '';
 
 export {
-  JWT_SECRET,
-  USER_ID,
-  USER_PASS,
   SERVER_PORT,
   BASE_PATH,
   API_ORIGIN,
