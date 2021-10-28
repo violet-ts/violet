112d19ee245003ff19f052f931cb65c0b51dcd58
Luma <world@luma.email>
2021-10-28 23:26:21 +0900

    fix: typo

--- server/utils/envValues.ts
+++ server/utils/envValues.ts
@@ -8,7 +8,7 @@ const API_ORIGIN = process.env.API_ORIGIN ?? ''
 const AWS_ACCESS_KEY_ID = process.env.MINIO_ROOT_USER ?? ''
 const AWS_SECRET_ACCESS_KEY = process.env.MINIO_ROOT_PASSWORD ?? ''
 const S3_REGION = process.env.S3_REGION ?? ''
-const S3_ENDPOINT = process.env.S3_REGION || undefined
+const S3_ENDPOINT = process.env.S3_ENDPOINT || undefined
 const S3_BUCKET = process.env.S3_BUCKET ?? 'violet-app'
 
 export {
