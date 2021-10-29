abb3c0cb21568de702babad93c9f771d3c59f00b
shuhei.ueki <kamarukul211@docomo.ne.jp>
2021-10-29 22:16:02 +0900

    fix:プルリクの指摘を修正

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,9 @@
+import type { DeskId, ProjectId } from '~/server/types'
+
 export const getWorkFullName = (work: { name: string; ext?: string | null }) =>
   `${work.name}${work.ext ? `.${work.ext}` : ''}`
+
+export const getProjectInfo = (path: string[]) => {
+  const [projectId, deskId] = path as [ProjectId, DeskId]
+  return { projectId, deskId }
+}
