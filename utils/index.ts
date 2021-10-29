6fe4863f58f7abf01e75b0c8b65b582cd19076c7
shuhei.ueki <kamarukul211@docomo.ne.jp>
2021-10-29 22:52:20 +0900

    fix:余計判定を削除

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,9 @@
-import type { DeskId, ProjectId } from '~/server/types'
+import type { ProjectId } from '~/server/types'
 
 export const getWorkFullName = (work: { name: string; ext?: string | null }) =>
   `${work.name}${work.ext ? `.${work.ext}` : ''}`
 
 export const getProjectInfo = (path: string[]) => {
-  const [projectId, deskId] = path as [ProjectId, DeskId]
-  return { projectId, deskId }
+  const [projectId, deskName] = path as [ProjectId, string]
+  return { projectId, deskName }
 }
