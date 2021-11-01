009bbd8cf9e8c0edf2815cfa804159abbe713870
Luma <world@luma.email>
2021-11-01 21:27:28 +0900

    fix: add arg API_ORIGIN; ビルド時に必要

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,9 +1,5 @@
 const withTM = require('next-transpile-modules')(['@violet/api'])
 
 module.exports = withTM({
-  env: {
-    API_ORIGIN: process.env.API_ORIGIN,
-    API_BASE_PATH: process.env.API_BASE_PATH,
-  },
   pageExtensions: ['page.tsx'],
 })
