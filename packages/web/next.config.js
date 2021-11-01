ff4d4aa8d9624dbb7c00b7cffc1cc38ffc2507e9
Luma <world@luma.email>
2021-11-01 20:26:53 +0900

    fix: env vars

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,6 +1,9 @@
 const withTM = require('next-transpile-modules')(['@violet/api'])
 
 module.exports = withTM({
-  env: {},
+  env: {
+    API_ORIGIN: process.env.API_ORIGIN,
+    API_BASE_PATH: process.env.API_BASE_PATH,
+  },
   pageExtensions: ['page.tsx'],
 })
