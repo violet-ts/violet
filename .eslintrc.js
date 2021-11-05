4cc5f800fa385e347547e1ebdcc21d54c9296049
Luma <world@luma.email>
2021-11-05 10:21:58 +0900

    chore(rule): esilnt ルール更新、import 制限と .eslintrc.js などへのルール適用

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
+const pkgs = ['api', 'def', 'lambda', 'scripts', 'web']
 module.exports = {
   root: true,
+  ignorePatterns: ['!*.js', '!*.cjs', '!*.mjs', '!*.ts'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -7,7 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'import'],
   parser: '@typescript-eslint/parser',
   env: {
     browser: true,
@@ -51,5 +53,26 @@ module.exports = {
       files: ['*.js'],
       rules: { '@typescript-eslint/no-var-requires': ['off'] },
     },
+    ...pkgs.map((pkg) => ({
+      files: [`pkg/${pkg}/**/*.js`, `pkg/${pkg}/**/*.ts`, `pkg/${pkg}/**/*.tsx`],
+      rules: {
+        'no-restricted-imports': [
+          'error',
+          {
+            patterns: [
+              {
+                group: [
+                  ...pkgs
+                    .filter((pkg2) => pkg2 !== pkg && pkg2 !== 'api' && pkg2 !== 'def')
+                    .map((pkg2) => `!@violet/${pkg2}`),
+                  ...(pkg === 'api' ? [] : ['@violet/api/src/*']),
+                ],
+                message: `only allowed to import modules under @violet/${pkg}, @violet/def, @violet/api/api and @violet/api/types`,
+              },
+            ],
+          },
+        ],
+      },
+    })),
   ],
 }
