76c132491e1452664f3ced36fe339f67946e1c74
Luma <world@luma.email>
2021-11-07 22:10:55 +0900

    chore: コミット漏れ

--- pkg/api/prisma/scripts/prod/0000-seed.ts
+++ pkg/api/prisma/scripts/prod/0000-seed.ts
@@ -1,5 +1,6 @@
 import type { PrismaClient } from '@prisma/client'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const main = async (_prisma: PrismaClient) => {
   console.warn('TODO: do something')
   // ...
