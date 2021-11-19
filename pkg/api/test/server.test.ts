f4c3551d571eaa6937619d4f9d6c6fb522531631
Luma <world@luma.email>
2021-11-20 02:01:53 +0900

    test: new fastify plugins injection

--- pkg/api/test/server.test.ts
+++ pkg/api/test/server.test.ts
@@ -28,6 +28,29 @@ test('dependency injection into controller', async () => {
   const message = 'test message'
   const res = await injectedController.get({
     query: { limit, message },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    logger: null as any,
+    cookies: {},
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    env: {} as any,
+    unsignCookie: () => {
+      throw new Error('not implemented')
+    },
+    generateCsrf: () => {
+      throw new Error('not implemented')
+    },
+    setCookie: () => {
+      throw new Error('not implemented')
+    },
+    getUserClaims: () => {
+      throw new Error('not implemented')
+    },
+    ensureUserClaims: () => {
+      throw new Error('not implemented')
+    },
+    refreshUserClaims: () => {
+      throw new Error('not implemented')
+    },
   })
 
   if (res.status !== 200) fail('Response must be successful')
