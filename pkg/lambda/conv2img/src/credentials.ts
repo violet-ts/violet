fbc15573fd2eab802243a5e356a6cbd174d6569d
Luma <world@luma.email>
2021-11-20 02:43:36 +0900

    chore(lambda-conv2img): fit to env name changes

--- pkg/lambda/conv2img/src/credentials.ts
+++ pkg/lambda/conv2img/src/credentials.ts
@@ -1,6 +1,6 @@
 import { fromEnv } from '@aws-sdk/credential-providers'
 import type { Credentials, Provider } from '@aws-sdk/types'
-import type { VioletEnv } from '@violet/def/envValues'
+import type { VioletEnv } from '@violet/def/env/violet'
 
 export const createCredentials = (env: VioletEnv): Credentials | Provider<Credentials> => {
   if (env.AWS_ACCESS_KEY_ID) {
