a107d7c37b2a2d6dd5a0bda50239e6e1566a7e26
mai_hirose <wind.close.cloud.girl@gmail.com>
2021-12-09 01:13:09 +0900

    chore: revert unnecessary modifications

--- pkg/lib/constants/file.ts
+++ pkg/lib/constants/file.ts
@@ -1,4 +1,4 @@
-export const FALLBACK_EXTS = ['jpg', 'png', 'webp'] as const
+export const FALLBACK_EXTS = ['jpg', 'png'] as const
 export const CONTENT_TYPES = {
   webp: 'image/webp',
   jpg: 'image/jpeg',
